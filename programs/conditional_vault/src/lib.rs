2cf296e125b9908913834ad86f90e8a1b9b4c8af
0xBrahma <0xBrahma@tutamail.com>
2022-09-15 23:30:12 +0000

    feat: add ability to create conditional token accounts

--- programs/conditional_vault/src/lib.rs
+++ programs/conditional_vault/src/lib.rs
@@ -6,10 +6,37 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod conditional_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_conditional_token_account(
+        ctx: Context<InitializeConditionalTokenAccount>, 
+        proposal_number: u64, 
+        redeemable_on_pass: bool
+    ) -> Result<()> {
+        let conditional_token_account = &mut ctx.accounts.conditional_token_account;
+
+        conditional_token_account.proposal_number = proposal_number;
+        conditional_token_account.redeemable_on_pass = redeemable_on_pass;
+        conditional_token_account.balance = 0;
+        conditional_token_account.authority = *ctx.accounts.authority.unsigned_key();
+
         Ok(())
     }
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeConditionalTokenAccount<'info> {
+    #[account(init, payer = authority, space = 8 + 8 + 1 + 8 + 32)]
+    conditional_token_account: Account<'info, ConditionalTokenAccount>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct ConditionalTokenAccount {
+    proposal_number: u64,
+    redeemable_on_pass: bool,
+    balance: u64,
+    authority: Pubkey,
+}
--- tests/conditionalVault.ts
+++ tests/conditionalVault.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { ConditionalVault } from "../target/types/conditional_vault";
+import { expect, assert } from "chai";
 
 describe("Conditional vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.ConditionalVault as Program<ConditionalVault>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Accounts can initialize conditional token accounts", async () => {
+    const conditionalTokenAccount = anchor.web3.Keypair.generate();
+    const proposalNumber = 564;
+    const redeemableOnPass = true;
+
+    await program.methods.initializeConditionalTokenAccount(new anchor.BN(proposalNumber), redeemableOnPass)
+    	.accounts({
+		    conditionalTokenAccount: conditionalTokenAccount.publicKey,
+		    authority: provider.wallet.publicKey,
+		    systemProgram: anchor.web3.SystemProgram.programId
+	})
+	.signers([conditionalTokenAccount])
+	.rpc();
+
+    const createdAccount = await program.account.conditionalTokenAccount.fetch(conditionalTokenAccount.publicKey);
+
+    assert.ok(createdAccount.balance.eq(new anchor.BN(0)));
+    assert.ok(createdAccount.proposalNumber.eq(new anchor.BN(proposalNumber)));
+    assert.ok(createdAccount.authority.equals(provider.wallet.publicKey)); 
+    assert.equal(createdAccount.redeemableOnPass, redeemableOnPass);
   });
+
+  it("Accounts can mint redeemable-on-pass tokens", async () => {
+    //
+  });
+
+  it("Accounts can mint redeemable-on-fail tokens", async () => {
+    //
+  });
+
+  it("Accounts cannot mint conditional tokens for a proposal that isn't active", async () => {
+    //
+  });
+
+  it("Accounts cannot mint conditional tokens with an insufficient normal token balance", async () => {
+    //
+  });
+
+  it("Accounts can burn redeemable-on-pass tokens", async () => {
+    //
+  });
+  it("Accounts can burn redeemable-on-fail tokens", async () => {
+    //
+  });
+
+  it("Accounts cannot burn conditional tokens before a proposal has passed or failed", async () => {
+    //
+  });
+
+
 });
