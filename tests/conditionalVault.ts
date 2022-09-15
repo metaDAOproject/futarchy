44b6d2d3425c60e071dc1b9038b0cbfd8e2747d9
0xBrahma <0xBrahma@tutamail.com>
2022-09-15 21:21:50 +0000

    chore: initialize conditional vault

--- tests/conditionalVault.ts
+++ tests/conditionalVault.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { MetaDao } from "../target/types/meta_dao";
+import { ConditionalVault } from "../target/types/conditional_vault";
 
-describe("metaDAO", () => {
+describe("Conditional vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.MetaDao as Program<MetaDao>;
+  const program = anchor.workspace.ConditionalVault as Program<ConditionalVault>;
 
   it("Is initialized!", async () => {
     // Add your test here.
