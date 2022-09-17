176e8d89c0b77ef2b972fb531f48972ab3f83152
0xBrahma <0xBrahma@tutamail.com>
2022-09-17 00:36:03 +0000

    feat: clarify data model and comment out core logic

--- programs/conditional_vault/src/lib.rs
+++ programs/conditional_vault/src/lib.rs
@@ -6,27 +6,58 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod conditional_vault {
     use super::*;
 
-    pub fn initialize_conditional_token_account(
-        ctx: Context<InitializeConditionalTokenAccount>, 
-        proposal_number: u64, 
-        redeemable_on_pass: bool
-    ) -> Result<()> {
-        let conditional_token_account = &mut ctx.accounts.conditional_token_account;
+    //pub fn initialize_conditional_token_account(
+    //    ctx: Context<InitializeConditionalTokenAccount>, 
+    //    proposal_number: u64, 
+    //    redeemable_on_pass: bool
+    //) -> Result<()> {
+    //    let conditional_token_account = &mut ctx.accounts.conditional_token_account;
 
-        conditional_token_account.proposal_number = proposal_number;
-        conditional_token_account.redeemable_on_pass = redeemable_on_pass;
-        conditional_token_account.balance = 0;
-        conditional_token_account.authority = *ctx.accounts.authority.unsigned_key();
+    //    conditional_token_account.proposal_number = proposal_number;
+    //    conditional_token_account.redeemable_on_pass = redeemable_on_pass;
+    //    conditional_token_account.balance = 0;
+    //    conditional_token_account.authority = *ctx.accounts.authority.unsigned_key();
 
-        Ok(())
-    }
+    //    Ok(())
+    //}
 
 
 }
 
+
+#[derive(Accounts)]
+pub struct InitializeConditionalExpression<'info> {
+    #[account(
+        init,
+        payer = initializer,
+        space = 8 + 8 + 1 // TODO: PDA
+    )]
+    conditional_expression: Account<'info, ConditionalExpression>,
+    #[account(mut)]
+    initializer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeConditionalVault<'info> {
+    #[account(
+        init,
+        payer = initializer,
+        space = 8 + 32 + 32
+    )]
+    conditional_vault: Account<'info, ConditionalVault>,
+    #[account(mut)]
+    initializer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
 #[derive(Accounts)]
 pub struct InitializeConditionalTokenAccount<'info> {
-    #[account(init, payer = authority, space = 8 + 8 + 1 + 8 + 32)]
+    #[account(
+        init, 
+        payer = authority, 
+        space = 8 + 32 + 8 + 8 + 32 
+    )]
     conditional_token_account: Account<'info, ConditionalTokenAccount>,
     #[account(mut)]
     authority: Signer<'info>,
@@ -34,9 +65,21 @@ pub struct InitializeConditionalTokenAccount<'info> {
 }
 
 #[account]
-pub struct ConditionalTokenAccount {
+pub struct ConditionalExpression {
     proposal_number: u64,
-    redeemable_on_pass: bool,
+    pass_or_fail_flag: bool, // true for tokens that are redeemable-on-pass, false for tokens that are redeemable-on-fail
+}
+
+#[account]
+pub struct ConditionalVault {
+    conditional_expression: Pubkey,
+    token_account: Pubkey,
+}
+
+#[account]
+pub struct ConditionalTokenAccount {
+    conditional_vault: Pubkey,
     balance: u64,
+    deposited_amount: u64,
     authority: Pubkey,
 }
--- tests/conditionalVault.ts
+++ tests/conditionalVault.ts
@@ -2,12 +2,17 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { ConditionalVault } from "../target/types/conditional_vault";
 import { expect, assert } from "chai";
+import * as token from "@solana/spl-token";
 
 describe("Conditional vault", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.ConditionalVault as Program<ConditionalVault>;
+
+  it("Can be initialized", async () => {
+    const conditionalVaultAccount = anchor.web3.Keypair.generate();
+
 
   it("Accounts can initialize conditional token accounts", async () => {
     const conditionalTokenAccount = anchor.web3.Keypair.generate();
@@ -31,9 +36,64 @@ describe("Conditional vault", () => {
     assert.equal(createdAccount.redeemableOnPass, redeemableOnPass);
   });
 
-  it("Accounts can mint redeemable-on-pass tokens", async () => {
-    //
-  });
+  //it("Accounts can mint redeemable-on-pass tokens", async () => {
+  //  const conditionalTokenAccount = anchor.web3.Keypair.generate();
+  //  const proposalNumber = 123;
+  //  const redeemableOnPass = true;
+
+  //  await program.methods.initializeConditionalTokenAccount(new anchor.BN(proposalNumber), redeemableOnPass)
+  //  	.accounts({
+  //      	    conditionalTokenAccount: conditionalTokenAccount.publicKey,
+  //      	    authority: provider.wallet.publicKey,
+  //      	    systemProgram: anchor.web3.SystemProgram.programId
+  //      })
+  //      .signers([conditionalTokenAccount])
+  //      .rpc();
+
+  //  const createdAccount = await program.account.conditionalTokenAccount.fetch(conditionalTokenAccount.publicKey);
+
+  //  assert.ok(createdAccount.balance.eq(new anchor.BN(0)));
+  //  assert.ok(createdAccount.proposalNumber.eq(new anchor.BN(proposalNumber)));
+  //  assert.ok(createdAccount.authority.equals(provider.wallet.publicKey)); 
+  //  assert.equal(createdAccount.redeemableOnPass, redeemableOnPass);
+
+  //  const mintAuthority = anchor.web3.Keypair.generate();
+
+  //  const mint = token.createMint(
+  //    provider.connection,
+  //    provider.wallet.payer,
+  //    mintAuthority.publicKey,
+  //    null,
+  //    2
+  //  );
+
+  //  // this is the depositor's normal token account i.e., the account that gets drained in the mint
+  //  const depositorTokenAccount = (await token.getOrCreateAssociatedTokenAccount(
+  //    connection,
+  //    provider.wallet.payer,
+  //    mint,
+  //    multisigSigner,
+  //    true
+  //  )).address;
+
+
+  //  const conditionalVaultTokenAccount = (await token.getOrCreateAssociatedTokenAccount(
+  //    connection,
+  //    provider.wallet.payer,
+  //    mint,
+  //    multisigSigner,
+  //    true
+  //  )).address;
+
+
+
+
+
+
+
+  //  
+  //  await program.methods.mintConditionalTokens
+  //});
 
   it("Accounts can mint redeemable-on-fail tokens", async () => {
     //
