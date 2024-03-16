901de337153483888f7fc23a378a68ddb1bd3590
metaproph3t <metaproph3t@protonmail.com>
2024-03-16 00:00:00 +0000

    Add `multi_transfer4` function to migrator

--- programs/autocrat_migrator/src/lib.rs
+++ programs/autocrat_migrator/src/lib.rs
@@ -2,8 +2,8 @@
 //! to the new autocrat.
 
 use anchor_lang::prelude::*;
-use anchor_spl::token::{transfer, Token, TokenAccount, Transfer};
 use anchor_lang::solana_program;
+use anchor_spl::token::{transfer, Token, TokenAccount, Transfer};
 
 declare_id!("migkwAXrXFN34voCYQUhFQBXZJjHrWnpEXbSGTqZdB3");
 
@@ -41,7 +41,10 @@ pub mod autocrat_migrator {
         let lamport_transfer = solana_program::system_instruction::transfer(
             &ctx.accounts.authority.key(),
             &ctx.accounts.lamport_receiver.key(),
-            ctx.accounts.authority.get_lamports().saturating_sub(rent.minimum_balance(0)),
+            ctx.accounts
+                .authority
+                .get_lamports()
+                .saturating_sub(rent.minimum_balance(0)),
         );
 
         solana_program::program::invoke(
@@ -49,7 +52,78 @@ pub mod autocrat_migrator {
             &[
                 ctx.accounts.authority.to_account_info(),
                 ctx.accounts.lamport_receiver.to_account_info(),
-            ]
+            ],
+        )?;
+
+        Ok(())
+    }
+
+    pub fn multi_transfer4(ctx: Context<MultiTransfer4>) -> Result<()> {
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.from0.to_account_info(),
+                    to: ctx.accounts.to0.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            ctx.accounts.from0.amount,
+        )?;
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.from1.to_account_info(),
+                    to: ctx.accounts.to1.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            ctx.accounts.from1.amount,
+        )?;
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.from2.to_account_info(),
+                    to: ctx.accounts.to2.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            ctx.accounts.from2.amount,
+        )?;
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.from3.to_account_info(),
+                    to: ctx.accounts.to3.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            ctx.accounts.from3.amount,
+        )?;
+
+        let rent = Rent::get()?;
+
+        let lamport_transfer = solana_program::system_instruction::transfer(
+            &ctx.accounts.authority.key(),
+            &ctx.accounts.lamport_receiver.key(),
+            ctx.accounts
+                .authority
+                .get_lamports()
+                .saturating_sub(rent.minimum_balance(0)),
+        );
+
+        solana_program::program::invoke(
+            &lamport_transfer,
+            &[
+                ctx.accounts.authority.to_account_info(),
+                ctx.accounts.lamport_receiver.to_account_info(),
+            ],
         )?;
 
         Ok(())
@@ -69,6 +143,33 @@ pub struct MultiTransfer2<'info> {
     from1: Account<'info, TokenAccount>,
     #[account(mut)]
     to1: Account<'info, TokenAccount>,
+    system_program: Program<'info, System>,
+    /// CHECK: no r/w, just lamport sub
+    #[account(mut)]
+    lamport_receiver: UncheckedAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct MultiTransfer4<'info> {
+    token_program: Program<'info, Token>,
+    #[account(mut)]
+    authority: Signer<'info>,
+    #[account(mut)]
+    from0: Account<'info, TokenAccount>,
+    #[account(mut)]
+    to0: Account<'info, TokenAccount>,
+    #[account(mut)]
+    from1: Account<'info, TokenAccount>,
+    #[account(mut)]
+    to1: Account<'info, TokenAccount>,
+    #[account(mut)]
+    from2: Account<'info, TokenAccount>,
+    #[account(mut)]
+    to2: Account<'info, TokenAccount>,
+    #[account(mut)]
+    from3: Account<'info, TokenAccount>,
+    #[account(mut)]
+    to3: Account<'info, TokenAccount>,
     system_program: Program<'info, System>,
     /// CHECK: no r/w, just lamport sub
     #[account(mut)]
