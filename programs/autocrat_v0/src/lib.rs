695cca0bece3c852ed6be7118bdeac3969a10f77
metaproph3t <metaproph3t@protonmail.com>
2024-04-03 00:00:00 +0000

    Start always start proposals at count = 1
    Bending the knee to the 1-indexer plebs ;(

--- programs/autocrat_v0/src/lib.rs
+++ programs/autocrat_v0/src/lib.rs
@@ -126,7 +126,7 @@ pub mod autocrat_v0 {
         dao.token_mint = ctx.accounts.token_mint.key();
         dao.usdc_mint = ctx.accounts.usdc_mint.key();
 
-        dao.proposal_count = 2;
+        dao.proposal_count = 1;
 
         dao.pass_threshold_bps = DEFAULT_PASS_THRESHOLD_BPS;
         dao.base_burn_lamports = DEFAULT_BASE_BURN_LAMPORTS;
