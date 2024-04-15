6f0a90c2df4a2ef253a16ee14662aaede777a7ac
metaproph3t <metaproph3t@protonmail.com>
2024-04-15 00:00:00 +0000

    Remove unused `print_error` macro

--- programs/amm/src/error.rs
+++ programs/amm/src/error.rs
@@ -7,14 +7,3 @@ pub enum ErrorCode {
     #[msg("Error in decimal scale conversion")]
     DecimalScaleError,
 }
-
-#[macro_export]
-macro_rules! print_error {
-    ($err:expr) => {{
-        || {
-            let error_code: ErrorCode = $err;
-            msg!("{:?} thrown at {}:{}", error_code, file!(), line!());
-            $err
-        }
-    }};
-}
