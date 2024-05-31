abc9a92cb2efc4110363765e94f859665aec26c6
metaproph3t <metaproph3t@protonmail.com>
2024-05-31 00:00:00 +0000

    Improve script for initialize proposal

--- sdk/src/utils/cu.ts
+++ sdk/src/utils/cu.ts
@@ -1,11 +1,11 @@
 export const MaxCUs = {
   initializeDao: 20_000,
-  createIdempotent: 25_000,
-  initializeConditionalVault: 45_000,
-  mintConditionalTokens: 35_000,
+  createIdempotent: 27_000,
+  initializeConditionalVault: 48_000,
+  mintConditionalTokens: 40_000,
   initializeAmm: 120_000,
   addLiquidity: 120_000,
   initializeProposal: 60_000,
 };
 
-export const DEFAULT_CU_PRICE = 1;
+export const DEFAULT_CU_PRICE = 100;
