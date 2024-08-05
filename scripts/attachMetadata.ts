cb6dad5bed509643624f6d88bd2ffbbde1a23310
Aiyualive <liuaiyu1302@gmail.com>
2024-08-05 02:22:56 -0600

    feat: skeleton of metadataattachment

--- scripts/attachMetadata.ts
+++ scripts/attachMetadata.ts
@@ -2,47 +2,84 @@ import { AutocratClient, ConditionalVaultClient } from "@metadaoproject/futarchy
 import * as anchor from "@coral-xyz/anchor";
 import { ComputeBudgetProgram, PublicKey } from "@solana/web3.js";
 import { DRIFT, USDC } from "./consts";
-import { MPL_TOKEN_METADATA_PROGRAM_ID } from "@metadaoproject/futarchy";
-
-const pDRIFT = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/7f095bfc-9f48-49b6-ca22-a5f2799c9a00/public";
-const fDRIFT = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/cb191bce-1f8b-4faf-fdc1-f80e73b84500/public";
-
-const pFUTURE = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/235d74f1-3750-4944-d97a-fc6d61955700/public";
-const fFUTURE = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/e69d51c5-fcfa-405e-657e-253185b4d800/public";
-
-const pUSDC = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/f38677ab-8ec6-4706-6606-7d4e0a3cfc00/public";
-const fUSDC = "https://imagedelivery.net/HYEnlujCFMCgj6yA728xIw/d9bfd8de-2937-419a-96f6-8d6a3a76d200/public";
-
-const proposal: PublicKey = new PublicKey("9jAnAupCdPQCFvuAMr5ZkmxDdEKqsneurgvUnx7Az9zS");
+import { ShadowProvider } from "./helpers/shadowProvider";
+import * as fs from "fs"
 
 let autocratClient: AutocratClient = AutocratClient.createClient({
   provider: anchor.AnchorProvider.env(),
 });
 
 let vaultClient: ConditionalVaultClient = autocratClient.vaultClient;
-
-async function main() {
-    const storedProposal = await autocratClient.getProposal(proposal);
+/**
+ *
+ * @param proposal
+ * @param proposalNumber
+ * @param baseToken
+ * @param quoteToken
+ * @param assetFolderPath
+ */
+async function main(
+    proposal: string,
+    proposalNumber: number,
+    baseToken: string,
+    assetFolderPath: string,
+    shdwAccount: string
+) {
+    const shdwAccountPubkey = new PublicKey(shdwAccount)
+    const storedProposal = await autocratClient.getProposal(new PublicKey(proposal));
 
     const { baseVault, quoteVault } = storedProposal;
-    console.log(baseVault);
 
-    
-    // Create shdw account if not provided
-    // Create 2 json for quotevault usdc w/ image
-    // Create 2 jsons for basevault <token> w/ image
-    // upload all 4 to shdw
-    // paste 4 into the function
+    const wallet = autocratClient.provider.wallet as anchor.Wallet
+    const shdwProvider = new ShadowProvider(autocratClient.provider.connection, wallet)
+    console.log("shdw", shdwProvider)
 
+    const pUSDCstring = "pUSDC"
+    const fUSDCstring = "fUSDC"
+    const pUSDCuri=  await shdwProvider.upsertFile({
+        name: pUSDCstring,
+        buffer: fs.readFileSync(`${assetFolderPath}/${pUSDCstring}`),
+        account: shdwAccountPubkey
+    })
 
+    const fUSDCuri=  await shdwProvider.upsertFile({
+        name: pUSDCstring,
+        buffer: fs.readFileSync(`${assetFolderPath}/${fUSDCstring}`),
+        account: shdwAccountPubkey
+    })
 
-    vaultClient.addMetadataToConditionalTokensIx(quoteVault, USDC, 1, pUSDC, fUSDC)
+    const pBaseString = `p${baseToken}`
+    const fBaseString = `f${baseToken}`
+    const pBaseUri=  await shdwProvider.upsertFile({
+        name: pBaseString,
+        buffer: fs.readFileSync(`${assetFolderPath}/${pBaseString}`),
+        account: shdwAccountPubkey
+    })
+
+    const fBaseUri=  await shdwProvider.upsertFile({
+        name: fBaseString,
+        buffer: fs.readFileSync(`${assetFolderPath}/${fBaseString}`),
+        account: shdwAccountPubkey
+    })
+
+    vaultClient.addMetadataToConditionalTokensIx(quoteVault, USDC, proposalNumber, pUSDCuri, fUSDCuri)
         .preInstructions([
-            await vaultClient.addMetadataToConditionalTokensIx(baseVault, DRIFT, 1, pDRIFT, fDRIFT).instruction(),
+            await vaultClient.addMetadataToConditionalTokensIx(baseVault, DRIFT, 1, pBaseUri, fBaseUri).instruction(),
             ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 10_000 }),
             ComputeBudgetProgram.setComputeUnitLimit({ units: 250_000 }),
         ])
         .rpc()
 }
 
-main();
+main(
+    // Proposal pubkey
+    process.argv[2],
+    // Proposal number
+    parseInt(process.argv[3]),
+    // Base token DRIFT
+    process.argv[4],
+    // AssetFolderPath
+    process.env[5],
+    // Shdw account key,
+    process.argv[6]
+  );
