043a6e1f775b72ec5c65db4359e13db13752095f
metaproph3t <metaproph3t@protonmail.com>
2024-09-13 00:00:00 +0000

    Update setup devnet script

--- scripts/setupDevnet.ts
+++ scripts/setupDevnet.ts
@@ -1,38 +1,122 @@
 import * as token from "@solana/spl-token";
 import { PublicKey } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import { ConditionalVaultAccount, ConditionalVaultClient, getQuestionAddr, getVaultAddr } from "@metadaoproject/futarchy/v0.4";
+import { AmmClient, ConditionalVault, ConditionalVaultClient, getAmmAddr, getQuestionAddr, getVaultAddr } from "@metadaoproject/futarchy/v0.4";
 import { sha256 } from "@metadaoproject/futarchy";
-import { Question } from "@metadaoproject/futarchy/v0.4";
+import { Question, Amm } from "@metadaoproject/futarchy/v0.4";
+import { BN } from "bn.js";
 
 
 const provider = anchor.AnchorProvider.env();
 const payer = provider.wallet["payer"];
 const vaultProgram: ConditionalVaultClient = ConditionalVaultClient.createClient({ provider });
+const ammProgram: AmmClient = AmmClient.createClient({ provider });
+
+const USDC = new PublicKey("CRWxbGNtVrTr9FAJX6SZpsvPZyi9R7VetuqecoZ1jCdD");
 
 async function main() {
-    const questionId = sha256(
+    const outcomeQuestionId = sha256(
         new TextEncoder().encode(
-            "Grant ID #1/E-UP/E-DOWN"
+            "Will Futarded Foundation fund a Go SDK for Anchor?/No/Yes"
         )
     );
-    const question = getQuestionAddr(vaultProgram.vaultProgram.programId, questionId, payer.publicKey, 2)[0];
-    const storedQuestion: Question | null = await vaultProgram.fetchQuestion(question);
+    const metricQuestionId = sha256(
+        new TextEncoder().encode(
+            "How effective will the Go SDK for Anchor be deemed?/Down/Up"
+        )
+    );
 
-    if (!storedQuestion) {
-        await vaultProgram.initializeQuestionIx(questionId, payer.publicKey, 2).rpc();
+    // await token.createAssociatedTokenAccount(provider.connection, payer, USDC, payer.publicKey);
+
+    let tokenAccount = token.getAssociatedTokenAddressSync(USDC, payer.publicKey);
+    // await token.mintTo(provider.connection, payer, USDC, tokenAccount,payer, 10 ** 6);
+
+
+    // let storedQuestion: Question | null = await vaultProgram.fetchQuestion(outcomeQuestion);
+
+    // for (const questionId of [outcomeQuestionId, metricQuestionId]) {
+    //     const question = getQuestionAddr(vaultProgram.vaultProgram.programId, questionId, payer.publicKey, 2)[0];
+    //     let storedQuestion: Question | null = await vaultProgram.fetchQuestion(question);
+    //     if (!storedQuestion) {
+    //         await vaultProgram.initializeQuestionIx(questionId, payer.publicKey, 2).rpc();
+    //         storedQuestion = await vaultProgram.fetchQuestion(question);
+    //         console.log(storedQuestion);
+    //     }
+    // }
+
+    const outcomeQuestion = getQuestionAddr(vaultProgram.vaultProgram.programId, outcomeQuestionId, payer.publicKey, 2)[0];
+    const metricQuestion = getQuestionAddr(vaultProgram.vaultProgram.programId, metricQuestionId, payer.publicKey, 2)[0];
+
+    let storedOutcomeQuestion: Question | null = await vaultProgram.fetchQuestion(outcomeQuestion);
+    if (!storedOutcomeQuestion) {
+        await vaultProgram.initializeQuestionIx(outcomeQuestionId, payer.publicKey, 2).rpc();
+        storedOutcomeQuestion = await vaultProgram.fetchQuestion(outcomeQuestion);
     }
 
-    const underlyingMint = await token.createMint(provider.connection, payer, payer.publicKey, null, 6);
-
-    const vault = getVaultAddr(vaultProgram.vaultProgram.programId, question, underlyingMint)[0];
-    const storedVault: ConditionalVaultAccount | null = await vaultProgram.fetchVault(vault);
-
-    if (!storedVault) {
-        await vaultProgram.initializeVaultIx(question, underlyingMint, 2).rpc();
+    let storedMetricQuestion: Question | null = await vaultProgram.fetchQuestion(metricQuestion);
+    if (!storedMetricQuestion) {
+        await vaultProgram.initializeQuestionIx(metricQuestionId, payer.publicKey, 2).rpc();
+        storedMetricQuestion = await vaultProgram.fetchQuestion(metricQuestion);
     }
 
-    console.log(await vaultProgram.fetchVault(vault));
+    console.log("OUTCOME QUESTION")
+    console.log(outcomeQuestion);
+    console.log(storedOutcomeQuestion);
+
+    console.log("METRIC QUESTION")
+    console.log(metricQuestion);
+    console.log(storedMetricQuestion);
+
+    const outcomeVault = getVaultAddr(vaultProgram.vaultProgram.programId, outcomeQuestion, USDC)[0];
+    let storedOutcomeVault: ConditionalVault | null = await vaultProgram.fetchVault(outcomeVault);
+
+    if (!storedOutcomeVault) {
+        await vaultProgram.initializeVaultIx(outcomeQuestion, USDC, 2).rpc();
+        storedOutcomeVault = await vaultProgram.fetchVault(outcomeVault);
+    }
+
+    const pUSDC = storedOutcomeVault.conditionalTokenMints[1];
+
+    const metricVault = getVaultAddr(vaultProgram.vaultProgram.programId, metricQuestion, pUSDC)[0];
+    let storedMetricVault: ConditionalVault | null = await vaultProgram.fetchVault(metricVault);
+
+    if (!storedMetricVault) {
+        await vaultProgram.initializeVaultIx(metricQuestion, pUSDC, 2).rpc();
+        storedMetricVault = await vaultProgram.fetchVault(metricVault);
+    }
+
+    console.log("Outcome Vault");
+    console.log(outcomeVault);
+    console.log(storedOutcomeVault);
+    console.log("Metric Vault");
+    console.log(metricVault);
+    console.log(storedMetricVault);
+
+    // await vaultProgram.splitTokensIx(outcomeQuestion, outcomeVault, USDC, new BN(1000 * 10 ** 6), 2).rpc();
+    // await vaultProgram.splitTokensIx(metricQuestion, metricVault, pUSDC, new BN(1000 * 10 ** 6), 2).rpc();
+
+    const pDown = storedMetricVault.conditionalTokenMints[0];
+    const pUp = storedMetricVault.conditionalTokenMints[1];
+
+    const amm = getAmmAddr(ammProgram.program.programId, pUp, pDown)[0];
+
+    console.log(amm)
+
+    let storedAmm: Amm | null = await ammProgram.fetchAmm(amm);
+    console.log(storedAmm);
+    if (!storedAmm) {
+        await ammProgram.initializeAmmIx(pUp, pDown, new BN(0), new BN(0)).rpc();
+        storedAmm = await ammProgram.fetchAmm(amm);
+    }
+
+    // await ammProgram.addLiquidity(amm, 1000, 1000);
+
+    // const amm = await ammProgram.fetchAmm(ammProgram.ammProgram.programId, pDown, pUp);
+
+
+    // console.log(storedVault);
+
+    // console.log(await vaultProgram.fetchVault(vault));
 
 }
 
